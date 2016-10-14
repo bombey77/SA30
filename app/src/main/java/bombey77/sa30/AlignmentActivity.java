d192db1c2ab416069507c6a81fdbc557bc6baf12
Ромашка <bombey77@mail.ru>
2016-10-14 20:46:16 +0300

    Initial commit

---
+++ app/src/main/java/bombey77/sa30/AlignmentActivity.java
@@ -0,0 +1,47 @@
+package bombey77.sa30;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+
+public class AlignmentActivity extends Activity implements View.OnClickListener{
+
+    Button btnLeft, btnCenter, btnRight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alignment);
+
+        btnLeft = (Button) findViewById(R.id.btnLeft);
+        btnCenter = (Button) findViewById(R.id.btnCenter);
+        btnRight = (Button) findViewById(R.id.btnRight);
+
+        btnLeft.setOnClickListener(this);
+        btnCenter.setOnClickListener(this);
+        btnRight.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent();
+
+        switch (v.getId()) {
+            case R.id.btnLeft:
+                intent.putExtra("align", Gravity.LEFT);
+                break;
+            case R.id.btnCenter:
+                intent.putExtra("align", Gravity.CENTER);
+                break;
+            case R.id.btnRight:
+                intent.putExtra("align", Gravity.RIGHT);
+                break;
+        }
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/bombey77/sa30/ColorActivity.java
@@ -0,0 +1,47 @@
+package bombey77.sa30;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ColorActivity extends Activity implements View.OnClickListener{
+
+    Button btnRed, btnBlue, btnGreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color);
+
+        btnRed = (Button) findViewById(R.id.btnRed);
+        btnBlue = (Button) findViewById(R.id.btnBlue);
+        btnGreen = (Button) findViewById(R.id.btnGreen);
+
+        btnRed.setOnClickListener(this);
+        btnBlue.setOnClickListener(this);
+        btnGreen.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent();
+
+        switch (v.getId()) {
+            case R.id.btnRed:
+                intent.putExtra("color", Color.RED);
+                break;
+            case R.id.btnBlue:
+                intent.putExtra("color", Color.BLUE);
+                break;
+            case R.id.btnGreen:
+                intent.putExtra("color", Color.GREEN);
+                break;
+        }
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
